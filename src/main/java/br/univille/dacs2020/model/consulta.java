c9d0f628a3c3de19821ec5c544fc4b958d47f21f
MATEUS WALZ <mateus.walz@univille.br>
2020-07-01 22:46:08 +0000

    versao1

---
+++ src/main/java/br/univille/dacs2020/model/consulta.java
@@ -0,0 +1,70 @@
+package br.univille.dacs2020.model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.fasterxml.jackson.databind.util.ClassUtil.Ctor;
+
+@Entity
+public class consulta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Temporal(value = TemporalType.DATE)
+    private Date data;
+    private String status;
+    
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.REFRESH})
+    private paciente paciente;
+
+    @OneToMany
+    @JoinColumn(name = "CONSULTA_ID")
+    private List <ProcedimentoRealizado> listaprocedimentos = new ArrayList<>();
+    
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public paciente getPaciente() {
+        return paciente;
+    }
+
+    public void setPaciente(paciente paciente) {
+        this.paciente = paciente;
+    }
+    
+}
