87f004aaea239bca982271f14d0408477d88e4e3
MATEUS WALZ <mateus.walz@univille.br>
2020-07-09 23:29:51 +0000

    commit

--- src/main/java/br/univille/dacs2020/controller/PacienteController.java
+++ src/main/java/br/univille/dacs2020/controller/PacienteController.java
@@ -5,11 +5,16 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.univille.dacs2020.model.Paciente;
 import br.univille.dacs2020.service.PacienteService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @Controller
@@ -24,4 +29,22 @@ public class PacienteController {
         List<Paciente> listaPacientes = service.getAll();
         return new ModelAndView("paciente/index","listapacientes",listaPacientes);
     }
+
+    @GetMapping("/novo")
+    public ModelAndView createForm(@ModelAttribute Paciente paciente) {
+        return new ModelAndView("paciente/form");
+    }
+    @PostMapping(params="form")
+    public ModelAndView save(Paciente paciente){
+        
+        service.save(paciente);
+        return new ModelAndView("redirect:/paciente");
+    }
+    @GetMapping(value="/alterar/{id}")
+    public ModelAndView edit(@PathVariable("id") Paciente paciente) {
+        return new ModelAndView("paciente/form","paciente",paciente);
+    }
+    
+    
+    
 }
